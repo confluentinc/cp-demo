03382a0af5c6dbbe7b359c54115d197ab31f6040
Yeva Byzek <yeva@confluent.io>
2018-01-16 20:05:55 -0500

    Removing unnecessary lines from create certs script

--- security/verify-certs.sh
+++ security/verify-certs.sh
@@ -8,7 +8,7 @@ set -o nounset \
 for i in kafka1 kafka2 client schemaregistry
 do
         echo "------------------------------- $i keystore -------------------------------"
-	keytool -list -v -keystore kafka.$i.keystore.jks -storepass confluent | grep Alias
+	keytool -list -v -keystore kafka.$i.keystore.jks -storepass confluent | grep -e Alias -e Entry
         echo "------------------------------- $i truststore -------------------------------"
-	keytool -list -v -keystore kafka.$i.truststore.jks -storepass confluent | grep Alias
+	keytool -list -v -keystore kafka.$i.truststore.jks -storepass confluent | grep -e Alias -e Entry
 done
