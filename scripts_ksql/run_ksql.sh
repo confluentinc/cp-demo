5eb39f5be115e4ee1559482e44f4477f5e2d0e02
Yeva Byzek <yeva@confluent.io>
2018-01-17 16:32:17 -0500

    Configure KSQL to use HTTP to SR instead of HTTPS

--- scripts_ksql/run_ksql.sh
+++ scripts_ksql/run_ksql.sh
@@ -2,4 +2,4 @@
 
 docker exec cpdemo_ksql-cli_1 ksql-server-start /tmp/ksqlproperties >/tmp/ksql.log 2>&1 &
 sleep 10
-#docker-compose exec ksql-cli ksql-cli remote http://localhost:8080 --exec "run script '/tmp/ksqlcommands';" 
+docker-compose exec ksql-cli ksql-cli remote http://localhost:8080 --exec "run script '/tmp/ksqlcommands';" 
