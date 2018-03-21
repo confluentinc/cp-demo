6cbf3d884c56381c34314a3e8584149e413217f7
Ramon Spahr <spahr@puzzle.ch>
2018-03-21 14:37:11 +0100

    add -r to xargs for handling null params

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -3,4 +3,4 @@
 ps aux | grep ksql-server-start | grep -v "grep ksql" | awk '{print $2}' | xargs kill -15
 
 docker-compose down
-docker volume ls -q --filter dangling=true | xargs docker volume rm
+docker volume ls -q --filter dangling=true | xargs -r docker volume rm
