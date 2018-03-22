a2fa43b8cd64eb1752570ed555069732f64972a5
Ramon Spahr <spahr@puzzle.ch>
2018-03-22 15:40:06 +0100

    change to iteration for docker volume rm

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -4,7 +4,6 @@ ps aux | grep ksql-server-start | grep -v "grep ksql" | awk '{print $2}' | xargs
 
 docker-compose down
 
-volumes=$(docker volume ls -q --filter="dangling=true")
-if [ -n "$volumes" ]; then
-	docker volumes rm "$volumes"
-fi
+for v in $(docker volume ls -q --filter="dangling=true"); do
+	docker volume rm "$v"
+done
