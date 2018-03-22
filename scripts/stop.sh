1ae77e16ba046b748c553043ee21452edacebb6b
Ramon Spahr <spahr@puzzle.ch>
2018-03-22 08:20:15 +0100

    replace xargs with proper check

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -3,4 +3,8 @@
 ps aux | grep ksql-server-start | grep -v "grep ksql" | awk '{print $2}' | xargs kill -15
 
 docker-compose down
-docker volume ls -q --filter dangling=true | xargs -r docker volume rm
+
+volumes=$(docker volume ls -q --filter="dangling=true")
+if [ -n "$volumes" ]; then
+	docker volumes rm "$volumes"
+fi
