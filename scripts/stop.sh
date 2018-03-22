48bf7354e75b9ae9244692ae0dc70ea233b7c1ae
Ramon Spahr <ramon.spahr@gmail.com>
2018-03-22 19:15:30 +0100

    improve stop.sh for deleting volumes (#38)
    * add -r to xargs for handling null params
    
    * replace xargs with proper check
    
    * change to iteration for docker volume rm

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -3,4 +3,7 @@
 ps aux | grep ksql-server-start | grep -v "grep ksql" | awk '{print $2}' | xargs kill -15
 
 docker-compose down
-docker volume ls -q --filter dangling=true | xargs docker volume rm
+
+for v in $(docker volume ls -q --filter="dangling=true"); do
+	docker volume rm "$v"
+done
