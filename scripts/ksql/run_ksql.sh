c9fbc3e1cd97bb3900e6098463f07737f75d506a
Yeva Byzek <yeva@confluent.io>
2018-04-18 22:33:08 -0400

    Upgrade Docker images to 4.1; modify how KSQL calls RUN SCRIPT

--- scripts/ksql/run_ksql.sh
+++ scripts/ksql/run_ksql.sh
@@ -2,4 +2,7 @@
 
 docker exec cpdemo_ksql-cli_1 ksql-server-start /tmp/ksqlproperties >/tmp/ksql.log 2>&1 &
 sleep 10
-docker-compose exec ksql-cli ksql-cli remote http://localhost:8080 --exec "run script '/tmp/ksqlcommands';" 
+docker-compose exec -T ksql-cli ksql http://localhost:8080 <<EOF
+run script '/tmp/ksqlcommands';
+exit ;
+EOF
