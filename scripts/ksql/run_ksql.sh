7704f5e073ec8cd2313f27a7a739085d8eef8f0f
Yeva Byzek <yeva@confluent.io>
2018-04-23 20:01:09 -0400

    #45: fix run script for linux

--- scripts/ksql/run_ksql.sh
+++ scripts/ksql/run_ksql.sh
@@ -2,7 +2,8 @@
 
 docker exec cpdemo_ksql-cli_1 ksql-server-start /tmp/ksqlproperties 2>&1 &
 sleep 10
-docker-compose exec -T ksql-cli ksql http://localhost:8088 <<EOF
+docker exec cpdemo_ksql-cli_1 bash -c "ksql http://localhost:8088 <<EOF
 run script '/tmp/ksqlcommands';
 exit ;
 EOF
+"
