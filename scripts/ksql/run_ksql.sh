ddad343e711ae43c08ffc916fc68993884c7c363
Yeva Byzek <yeva@confluent.io>
2018-06-26 14:21:15 -0400

    Update master to run for 5.0.x

--- scripts/ksql/run_ksql.sh
+++ scripts/ksql/run_ksql.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-docker exec ksql-cli ksql-server-start /tmp/ksqlproperties 2>&1 &
-sleep 10
-docker exec ksql-cli bash -c "ksql http://localhost:8088 <<EOF
+
+docker-compose exec ksql-cli  bash -c "ksql http://ksql-server:8088 <<EOF
 run script '/tmp/ksqlcommands';
 exit ;
 EOF
