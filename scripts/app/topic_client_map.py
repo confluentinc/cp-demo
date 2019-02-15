df6c567f411b8fce5d2cf000cc85211952638b42
Yeva Byzek <yeva@confluent.io>
2019-02-14 19:21:11 -0500

    name the connect-worker-producer

--- scripts/app/topic_client_map.py
+++ scripts/app/topic_client_map.py
@@ -12,26 +12,65 @@
 # Usage
 # -----
 # ./topic_client_map.py
-
+#
 # Sample output
 # -------------
 #
+# Reading topic _confluent-monitoring for 60 seconds...please wait
+# 
 # WIKIPEDIABOT:
 # - producers:
-#     _confluent-ksql-default_query_CSAS_WIKIPEDIABOT_1-053e85c1-b4f0-4d17-a412-1ca2f9b8043a-StreamThread-7-producer
-#     _confluent-ksql-default_query_CSAS_WIKIPEDIABOT_1-053e85c1-b4f0-4d17-a412-1ca2f9b8043a-StreamThread-8-producer
-#     _confluent-ksql-default_query_CSAS_WIKIPEDIABOT_1-053e85c1-b4f0-4d17-a412-1ca2f9b8043a-StreamThread-6-producer
+#     _confluent-ksql-default_query_CSAS_WIKIPEDIABOT_1-d3bf43ec-0fb2-4919-9bed-d5941969dfbc-StreamThread-8-producer
+#     _confluent-ksql-default_query_CSAS_WIKIPEDIABOT_1-d3bf43ec-0fb2-4919-9bed-d5941969dfbc-StreamThread-6-producer
 # - consumers:
 #     connect-elasticsearch-ksql
-#
+# 
 # wikipedia.parsed:
 # - producers:
-#     producer-6
+#     connect-worker-producer
 # - consumers:
-#     _confluent-ksql-default_query_CSAS_WIKIPEDIABOT_1
+#     _confluent-ksql-default_query_CSAS_WIKIPEDIANOBOT_0
 #     _confluent-ksql-default_query_CTAS_EN_WIKIPEDIA_GT_1_2
 #     connect-replicator
-#     _confluent-ksql-default_query_CSAS_WIKIPEDIANOBOT_0
+#     _confluent-ksql-default_query_CSAS_WIKIPEDIABOT_1
+# 
+# wikipedia.parsed.replica:
+# - producers:
+#     connect-worker-producer
+# - consumers:
+# 
+# WIKIPEDIANOBOT:
+# - producers:
+#     _confluent-ksql-default_query_CSAS_WIKIPEDIANOBOT_0-f4141302-3f36-4c42-95ab-04012aa64be0-StreamThread-3-producer
+#     _confluent-ksql-default_query_CSAS_WIKIPEDIANOBOT_0-f4141302-3f36-4c42-95ab-04012aa64be0-StreamThread-4-producer
+# - consumers:
+#     WIKIPEDIANOBOT-consumer
+# 
+# _confluent-ksql-default_query_CTAS_EN_WIKIPEDIA_GT_1_2-KSTREAM-AGGREGATE-STATE-STORE-0000000007-repartition:
+# - producers:
+#     _confluent-ksql-default_query_CTAS_EN_WIKIPEDIA_GT_1_2-32cee64d-64dd-4fa9-b21b-e5bb7a0aed0a-StreamThread-12-producer
+# - consumers:
+#     _confluent-ksql-default_query_CTAS_EN_WIKIPEDIA_GT_1_2
+# 
+# EN_WIKIPEDIA_GT_1:
+# - producers:
+#     _confluent-ksql-default_query_CTAS_EN_WIKIPEDIA_GT_1_2-32cee64d-64dd-4fa9-b21b-e5bb7a0aed0a-StreamThread-9-producer
+#     _confluent-ksql-default_query_CTAS_EN_WIKIPEDIA_GT_1_2-32cee64d-64dd-4fa9-b21b-e5bb7a0aed0a-StreamThread-11-producer
+# - consumers:
+#     _confluent-ksql-default_query_CSAS_EN_WIKIPEDIA_GT_1_COUNTS_3
+# 
+# _confluent-ksql-default_query_CTAS_EN_WIKIPEDIA_GT_1_2-KSTREAM-AGGREGATE-STATE-STORE-0000000007-changelog:
+# - producers:
+#     _confluent-ksql-default_query_CTAS_EN_WIKIPEDIA_GT_1_2-32cee64d-64dd-4fa9-b21b-e5bb7a0aed0a-StreamThread-9-producer
+#     _confluent-ksql-default_query_CTAS_EN_WIKIPEDIA_GT_1_2-32cee64d-64dd-4fa9-b21b-e5bb7a0aed0a-StreamThread-11-producer
+# - consumers:
+# 
+# EN_WIKIPEDIA_GT_1_COUNTS:
+# - producers:
+#     _confluent-ksql-default_query_CSAS_EN_WIKIPEDIA_GT_1_COUNTS_3-232a4aa1-f2cf-43a3-8c73-10b0410855da-StreamThread-13-producer
+#     _confluent-ksql-default_query_CSAS_EN_WIKIPEDIA_GT_1_COUNTS_3-232a4aa1-f2cf-43a3-8c73-10b0410855da-StreamThread-15-producer
+# - consumers:
+#     EN_WIKIPEDIA_GT_1_COUNTS-consumer
 #
 #########################
 
