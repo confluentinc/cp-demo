21a789ba226a408eff54eb9db345c061335afa65
Yeva Byzek <yeva@confluent.io>
2019-04-17 12:19:06 -0400

    DEVX-16: adjust cpus; add debugs

--- scripts/start.sh
+++ scripts/start.sh
@@ -27,6 +27,8 @@ fi
 # Stop existing demo Docker containers
 ${DIR}/stop.sh
 
+echo "DEBUG: $(top -l 1 -n 1 | grep Load )"
+
 # Generate keys and certificates used for SSL
 echo -e "Generate keys and certificates used for SSL"
 (cd ${DIR}/security && ./certs-create.sh)
@@ -46,7 +48,10 @@ while [[ ! $(docker-compose logs control-center) =~ "Started NetworkTrafficServe
     echo -e "\nERROR: The logs in control-center container do not show 'Started NetworkTrafficServerConnector' after $MAX_WAIT seconds. Please troubleshoot with 'docker-compose ps' and 'docker-compose logs'.\n"
     exit 1
   fi
+  echo "DEBUG: waiting for Control Center $CUR_WAIT"
 done
+
+echo "DEBUG: $(top -l 1 -n 1 | grep Load )"
 
 # Verify Docker containers started
 if [[ $(docker-compose ps) =~ "Exit 137" ]]; then
@@ -77,6 +82,7 @@ while [[ ! $(docker-compose logs connect) =~ "Herder started" ]]; do
     echo -e "\nERROR: The logs in Kafka Connect container do not show 'Herder started'. Please troubleshoot with 'docker-compose ps' and 'docker-compose logs'.\n"
     exit 1
   fi
+  echo "DEBUG: waiting for Herder $CUR_WAIT"
 done
 
 if [[ ! $(docker-compose exec connect timeout 3 nc -zv irc.wikimedia.org 6667) =~ "open" ]]; then
@@ -131,3 +137,5 @@ curl -X POST -H "Content-Type: application/json" -d '{"name":"Under Replicated P
 curl -X POST -H "Content-Type: application/json" -d '{"name":"Email Administrator","enabled":true,"triggerGuid":["'$(curl -X GET http://localhost:9021/2.0/alerts/triggers/ | jq --raw-output '.[0].guid')'","'$(curl -X GET http://localhost:9021/2.0/alerts/triggers/ | jq --raw-output '.[1].guid')'"],"maxSendRate":1,"intervalMs":"60000","email":{"address":"devnull@confluent.io","subject":"Confluent Control Center alert"}}' http://localhost:9021/2.0/alerts/actions
 
 echo -e "\nDONE! Connect to Confluent Control Center at http://localhost:9021\n"
+
+echo "DEBUG: $(top -l 1 -n 1 | grep Load )"
