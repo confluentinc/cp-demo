55c2725b818aefe2ece702b6063fe5d7e6ae62c9
Rick Spurgeon <rspurgeon@users.noreply.github.com>
2019-07-17 11:34:50 -0500

    DEVX-562: Disable provenance header explicitly (defaults to true <= 5.0)

--- scripts/connectors/submit_replicator_config.sh
+++ scripts/connectors/submit_replicator_config.sh
@@ -50,7 +50,8 @@ DATA=$( cat << EOF
     "src.kafka.timestamps.producer.confluent.monitoring.interceptor.ssl.keystore.password": "confluent",
     "src.kafka.timestamps.producer.confluent.monitoring.interceptor.sasl.jaas.config": "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"client\" password=\"client-secret\";",
     "src.kafka.timestamps.producer.confluent.monitoring.interceptor.sasl.mechanism": "PLAIN",
-    "tasks.max": "1"
+    "tasks.max": "1",
+    "provenance.header.enable": "false"
   }
 }
 EOF
