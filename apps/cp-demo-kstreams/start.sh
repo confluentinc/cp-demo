8d59cb76f4d2a9862d079d36b205a288347c0d27
Rick Spurgeon <rspurgeon@users.noreply.github.com>
2019-08-21 17:49:54 -0500

    DEVX-819 remove cp-demo-kstreams app

--- apps/cp-demo-kstreams/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-/usr/local/bin/dub template /etc/confluent/docker/kafka.properties.template /etc/kafka/kafka-streams.properties
-java $JAVA_OPTS $KAFKA_OPTS -jar /app/cp-demo-kstreams.jar /etc/kafka/kafka-streams.properties
-
