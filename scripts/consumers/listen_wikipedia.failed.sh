22486101c428cc6fd8e2699acbe405376fd355a1
Yeva Byzek <yeva@confluent.io>
2019-11-22 21:18:43 -0500

    Fix consumers

--- scripts/consumers/listen_wikipedia.failed.sh
+++ scripts/consumers/listen_wikipedia.failed.sh
@@ -2,5 +2,10 @@
 
 docker exec connect kafka-avro-console-consumer \
   --property schema.registry.url=https://schemaregistry:8085 \
+  --property schema.registry.ssl.truststore.location=/etc/kafka/secrets/kafka.client.truststore.jks \
+  --property schema.registry.ssl.truststore.password=confluent \
+  --property schema.registry.ssl.keystore.location=/etc/kafka/secrets/kafka.client.keystore.jks \
+  --property schema.registry.ssl.keystore.password=confluent  \
+  --property schema.registry.ssl.protocol=TLS \
   --bootstrap-server kafka1:9091 --topic wikipedia.failed \
   --consumer.config /etc/kafka/secrets/client_without_interceptors.config
