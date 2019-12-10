c73ba6d9901c9f040cc9fa6e77015d8518b7a16a
Rick Spurgeon <rspurgeon@users.noreply.github.com>
2019-12-10 08:23:02 -0600

    DEVX-1032 Addresses warnings in key scripting (#133)

--- scripts/ksql/run_ksql.sh
+++ scripts/ksql/run_ksql.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-
-docker-compose exec ksql-cli  bash -c "ksql http://ksql-server:8088 <<EOF
-run script '/tmp/ksqlcommands';
-exit ;
-EOF
-"
+docker-compose exec ksql-cli bash -c "/tmp/run_ksql_commands.sh"
