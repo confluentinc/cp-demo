653bf425621740c6971ad202715b54d83171d805
Yeva Byzek <yeva@confluent.io>
2020-04-09 08:00:11 -0400

    Remove jmxStack code

--- scripts/start.sh
+++ scripts/start.sh
@@ -9,11 +9,6 @@ preflight_checks || exit
 # Stop existing demo Docker containers
 ${DIR}/stop.sh
 
-jmxStack=$1
-if [[ ! -z "$jmxStack" ]]; then
-  setup_jmx $jmxStack
-fi
-
 # Generate keys and certificates used for SSL
 echo -e "Generate keys and certificates used for SSL"
 (cd ${DIR}/security && ./certs-create.sh)
