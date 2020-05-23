5710773566707899b9fcd8e9cd167fcbc54b66e7
Yeva Byzek <yeva@confluent.io>
2020-05-22 21:38:29 -0400

    Special branch for https://github.com/confluentinc/ksql/issues/4029

--- scripts/ksql/run_ksql.sh
+++ scripts/ksql/run_ksql.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 # Cannot reliably use 'run script' until https://github.com/confluentinc/ksql/issues/4029 is resolved
-docker-compose exec ksqldb-cli bash -c "/tmp/run_ksql_commands.sh"
+#docker-compose exec ksqldb-cli bash -c "/tmp/run_ksql_commands.sh"
+docker-compose exec ksqldb-cli  bash -c "ksql -u ksqlDBUser -p ksqlDBUser http://ksqldb-server:8088 <<EOF
+run script '/tmp/statements.sql';
+exit ;
+EOF
+"
