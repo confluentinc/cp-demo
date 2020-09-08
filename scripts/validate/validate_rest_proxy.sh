0f4406dff66a67481843c570c4046383e0d02007
Ryan P <ryan@confluent.io>
2020-09-08 10:02:14 -0400

    Update scripts/validate/validate_rest_proxy.sh
    Co-authored-by: Yeva Byzek <ybyzek@users.noreply.github.com>

--- scripts/validate/validate_rest_proxy.sh
+++ scripts/validate/validate_rest_proxy.sh
@@ -3,7 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 ################################## GET KAFKA CLUSTER ID ########################
-KAFKA_CLUSTER_ID=$(curl -s http://kafka1:8091/v1/metadata/id | jq -r ".id")
+KAFKA_CLUSTER_ID=$(curl -s http://localhost:8091/v1/metadata/id | jq -r ".id")
 if [ -z "$KAFKA_CLUSTER_ID" ]; then
     echo "Failed to retrieve Kafka cluster id"
     exit 1
