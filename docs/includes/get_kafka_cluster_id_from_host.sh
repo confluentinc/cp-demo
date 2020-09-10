4feb882002a4c2f17a12cf72fd4b0fca32ef045c
Yeva Byzek <yeva@confluent.io>
2020-09-09 21:40:10 -0400

    Simplify getting Kafka cluster ID

--- docs/includes/get_kafka_cluster_id_from_host.sh
+++ docs/includes/get_kafka_cluster_id_from_host.sh
@@ -1,1 +1,1 @@
-KAFKA_CLUSTER_ID=$(curl -s -k https://localhost:8091/v1/metadata/id --cert scripts/security/mds.certificate.pem --key scripts/security/mds.key --tlsv1.2 --cacert scripts/security/snakeoil-ca-1.crt | jq -r ".id")
+KAFKA_CLUSTER_ID=$(curl -s https://localhost:8091/v1/metadata/id --tlsv1.2 --cacert scripts/security/snakeoil-ca-1.crt | jq -r ".id")
