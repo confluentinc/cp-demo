541a3033bc660cffe0bc0845324b6c7389357347
Yeva Byzek <yeva@confluent.io>
2020-09-23 11:59:48 -0400

    DEVX-1809: checkpoint, switch from ZK to bootstrap servers for topic creation; retire kafka-client Docker container

--- scripts/security/certs-create.sh
+++ scripts/security/certs-create.sh
@@ -11,7 +11,7 @@ rm -f *.crt *.csr *_creds *.jks *.srl *.key *.pem *.der *.p12
 # Generate CA key
 openssl req -new -x509 -keyout snakeoil-ca-1.key -out snakeoil-ca-1.crt -days 365 -subj '/CN=ca1.test.confluentdemo.io/OU=TEST/O=CONFLUENT/L=PaloAlto/ST=Ca/C=US' -passin pass:confluent -passout pass:confluent
 
-for i in kafka1 kafka2 client schemaregistry restproxy connect controlcenter ksqlDBserver appSA badapp clientListen zookeeper mds
+for i in kafka1 kafka2 client schemaregistry restproxy connect controlcenter ksqlDBserver ksqlDBUser appSA badapp clientListen zookeeper mds
 do
   echo "------------------------------- $i -------------------------------"
   ./certs-create-per-user.sh $i
