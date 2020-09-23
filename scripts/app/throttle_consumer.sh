0f0f91aa1a42a392b694d5c22bff6f72b1a430e9
Yeva Byzek <yeva@confluent.io>
2020-09-23 12:59:10 -0400

    Remove ZK from kafka-configs

--- scripts/app/throttle_consumer.sh
+++ scripts/app/throttle_consumer.sh
@@ -18,11 +18,11 @@ fi
 
 CONSUMER_GROUP="app"
 
-echo "docker-compose exec kafka1 kafka-configs --zookeeper zookeeper:2181 --entity-type clients --entity-name consumer_app_$ID --alter $CONFIG"
-docker-compose exec kafka1 bash -c "kafka-configs --zookeeper zookeeper:2181 --entity-type clients --entity-name consumer_app_$ID --alter $CONFIG"
+echo "docker-compose exec kafka1 kafka-configs --bootstrap-server kafka1:12091 --entity-type clients --entity-name consumer_app_$ID --alter $CONFIG"
+docker-compose exec kafka1 bash -c "kafka-configs --bootstrap-server kafka1:12091 --entity-type clients --entity-name consumer_app_$ID --alter $CONFIG"
 
-echo "docker-compose exec kafka1 kafka-configs --zookeeper zookeeper:2181 --entity-type clients --describe"
-docker-compose exec kafka1 bash -c 'kafka-configs --zookeeper zookeeper:2181 --entity-type clients --describe'
+echo "docker-compose exec kafka1 kafka-configs --bootstrap-server kafka1:12091 --entity-type clients --describe"
+docker-compose exec kafka1 bash -c 'kafka-configs --bootstrap-server kafka1:12091 --entity-type clients --describe'
 
-echo "docker-compose exec kafka1 kafka-consumer-groups --bootstrap-server kafka1:9091 --describe --group $CONSUMER_GROUP --command-config /etc/kafka/secrets/client_without_interceptors.config"
-docker-compose exec kafka1 kafka-consumer-groups --bootstrap-server kafka1:9091 --describe --group $CONSUMER_GROUP --command-config /etc/kafka/secrets/client_without_interceptors.config
+echo "docker-compose exec kafka1 kafka-consumer-groups --bootstrap-server kafka1:12091 --describe --group $CONSUMER_GROUP"
+docker-compose exec kafka1 kafka-consumer-groups --bootstrap-server kafka1:12091 --describe --group $CONSUMER_GROUP
