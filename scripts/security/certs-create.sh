428de6cc5c577f8876937b3890242de2900a0bf1
Yeva Byzek <yeva@confluent.io>
2020-10-13 00:47:55 -0400

    Working with proper cacerts file -- thanks to Brett

--- scripts/security/certs-create.sh
+++ scripts/security/certs-create.sh
@@ -11,9 +11,6 @@ rm -f *.crt *.csr *_creds *.jks *.srl *.key *.pem *.der *.p12
 # Generate CA key
 openssl req -new -x509 -keyout snakeoil-ca-1.key -out snakeoil-ca-1.crt -days 365 -subj '/CN=ca1.test.confluentdemo.io/OU=TEST/O=CONFLUENT/L=PaloAlto/ST=Ca/C=US' -passin pass:confluent -passout pass:confluent
 
-# Restore docker-ca-bundle.crt from backup
-cp docker-ca-bundle.crt.backup docker-ca-bundle.crt
-
 for i in kafka1 kafka2 client schemaregistry restproxy connect connectorSA controlcenter ksqlDBserver ksqlDBUser appSA badapp clientListen zookeeper mds
 do
   echo "------------------------------- $i -------------------------------"
