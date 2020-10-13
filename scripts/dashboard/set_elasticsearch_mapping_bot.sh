71a747262253230236eec0385bc5cfdc8ef0b003
Yeva Byzek <yeva@confluent.io>
2020-10-13 03:57:00 -0400

    First attempt at fixing elasticsearch mappings

--- scripts/dashboard/set_elasticsearch_mapping_bot.sh
+++ scripts/dashboard/set_elasticsearch_mapping_bot.sh
@@ -9,34 +9,19 @@ DATA=$( cat << EOF
     "mappings": {
         "wikichange": {
             "properties": {
-                "CREATEDAT": {
+                "META.DT": {
                     "type": "date"
                 },
-                "WIKIPAGE": {
+                "URI": {
                     "type": "keyword"
                 },
-                "ISNEW": {
+                "BOT": {
                     "type": "boolean"
-                },
-                "ISMINOR": {
-                    "type": "boolean"
-                },
-                "ISUNPATROLLED": {
-                    "type": "boolean"
-                },
-                "ISBOT": {
-                    "type": "boolean"
-                },
-                "DIFFURL": {
-                    "type": "text"
                 },
                 "USERNAME": {
                     "type": "keyword"
                 },
-                "BYTECHANGE": {
-                    "type": "integer"
-                },
-                "COMMITMESSAGE": {
+                "COMMENT": {
                     "type": "text"
                 }
             }
