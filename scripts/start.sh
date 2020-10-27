086fc7d73d7e2f742795cb58e058bb61fcbeac6b
Yeva Byzek <yeva@confluent.io>
2020-10-26 21:14:50 -0400

    Tweak

--- scripts/start.sh
+++ scripts/start.sh
@@ -141,7 +141,7 @@ echo
 MAX_WAIT=30
 echo "Waiting up to $MAX_WAIT seconds for ksqlDB server to start"
 retry $MAX_WAIT host_check_ksqlDBserver_up || exit 1
-echo -e "\n\nRun ksqlDB queries (will take ~1 minute to return):"
+echo -e "\n\nRun ksqlDB queries:"
 ${DIR}/ksqlDB/run_ksqlDB.sh
 
 echo -e "\nStart consumers for additional topics: WIKIPEDIANOBOT, EN_WIKIPEDIA_GT_1_COUNTS"
