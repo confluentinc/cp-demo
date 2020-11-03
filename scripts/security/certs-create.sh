f5697528f13a9fb67151ab47feb6a2bf8d9066d1
Brett Randall <brett.randall@confluent.io>
2020-11-03 22:50:36 +1100

    Use same cert for ksqldb-server as for controlcenter.

--- scripts/security/certs-create.sh
+++ scripts/security/certs-create.sh
@@ -11,7 +11,12 @@ rm -f *.crt *.csr *_creds *.jks *.srl *.key *.pem *.der *.p12 *.log
 # Generate CA key
 openssl req -new -x509 -keyout snakeoil-ca-1.key -out snakeoil-ca-1.crt -days 365 -subj '/CN=ca1.test.confluentdemo.io/OU=TEST/O=CONFLUENT/L=PaloAlto/ST=Ca/C=US' -passin pass:confluent -passout pass:confluent
 
-users=(kafka1 kafka2 client schemaregistry restproxy connect connectorSA controlcenter ksqlDBserver ksqlDBUser appSA badapp clientListen zookeeper mds)
+# we no longer generating a cert for ksqlDB Server - it shares a cert with Control Center
+users=(kafka1 kafka2 client schemaregistry restproxy connect connectorSA controlcenter ksqlDBUser appSA badapp clientListen zookeeper mds)
 echo "Creating certificates"
 printf '%s\0' "${users[@]}" | xargs -0 -I{} -n1 -P15 sh -c './certs-create-per-user.sh "$1" > "certs-create-$1.log" 2>&1 && echo "Created certificates for $1"' -- {}
 echo "Creating certificates completed"
+
+# we no longer generating a cert for ksqlDB Server - it shares a cert with Control Center
+cp kafka.controlcenter.keystore.jks kafka.ksqlDBserver.keystore.jks
+cp kafka.controlcenter.truststore.jks kafka.ksqlDBserver.truststore.jks
