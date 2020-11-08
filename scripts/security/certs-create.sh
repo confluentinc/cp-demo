cafc11d902975985f0851f0f330709964aecb51b
Brett Randall <brett.randall@confluent.io>
2020-11-09 09:57:52 +1100

    Renamed cert and SAN from controlcenter to control-center to match hostname.

--- scripts/security/certs-create.sh
+++ scripts/security/certs-create.sh
@@ -11,7 +11,7 @@ rm -f *.crt *.csr *_creds *.jks *.srl *.key *.pem *.der *.p12 *.log
 # Generate CA key
 openssl req -new -x509 -keyout snakeoil-ca-1.key -out snakeoil-ca-1.crt -days 365 -subj '/CN=ca1.test.confluentdemo.io/OU=TEST/O=CONFLUENT/L=PaloAlto/ST=Ca/C=US' -passin pass:confluent -passout pass:confluent
 
-users=(kafka1 kafka2 client schemaregistry restproxy connect connectorSA controlcenter ksqlDBserver ksqlDBUser appSA badapp clientListen zookeeper mds)
+users=(kafka1 kafka2 client schemaregistry restproxy connect connectorSA control-center ksqlDBserver ksqlDBUser appSA badapp clientListen zookeeper mds)
 echo "Creating certificates"
 printf '%s\0' "${users[@]}" | xargs -0 -I{} -n1 -P15 sh -c './certs-create-per-user.sh "$1" > "certs-create-$1.log" 2>&1 && echo "Created certificates for $1"' -- {}
 echo "Creating certificates completed"
