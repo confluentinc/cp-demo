4844635fb4bdb09863a7c938242926f0d8f2143d
Brett Randall <brett.randall@confluent.io>
2020-12-07 14:26:31 +1100

    Update scripts/env.sh
    Co-authored-by: Yeva Byzek <ybyzek@users.noreply.github.com>

--- scripts/env.sh
+++ scripts/env.sh
@@ -13,6 +13,7 @@ export REPOSITORY=${REPOSITORY:-confluentinc}
 # and expects user to build and provide a local file confluentinc-kafka-connect-replicator-${CONNECTOR_VERSION}.zip
 export CONNECTOR_VERSION=${CONNECTOR_VERSION:-$CONFLUENT}
 
+# Set consistent and strong cipher suites across all services
 export SSL_CIPHER_SUITES=TLS_AES_256_GCM_SHA384,TLS_CHACHA20_POLY1305_SHA256,TLS_AES_128_GCM_SHA256,TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256
 
 # ZooKeeper image doesn't support TLS 1.3, so provide a couple of strong TLS 1.2 cipher suites
