8f68a86c3735d73cffc6dc9527c8ea3be8d9ac5c
Rick Spurgeon <rspurgeon@users.noreply.github.com>
2021-06-29 13:43:04 -0500

    DEVX-2622 & DEVX-2640: Update kstreams deps to CP versions and redact logging (#377)
    * get object values in property building
    
    * DEVX-2622 DEVX-2640: Update kstreams-app to late model kstreams and disable certain logging
    
    * DEVX-2622: kstreams update fallback version to 6.2.0

--- kstreams-app/src/main/jib/app/start.sh
+++ kstreams-app/src/main/jib/app/start.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
 /usr/local/bin/dub template /etc/confluent/docker/kafka.properties.template /etc/kafka/kafka-streams.properties
-echo $JAVA_OPTS
-echo $KAFKA_OPTS
-set
 java $JAVA_OPTS $KAFKA_OPTS -cp /app/resources:/app/classes:/app/libs/* io.confluent.demos.common.wiki.WikipediaActivityMonitor "/etc/kafka/kafka-streams.properties"
 
