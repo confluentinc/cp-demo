1c0e1d1d09fc8485c1c5e9e08f5779f28b27b864
Daniel Petisme <daniel.petisme@gmail.com>
2021-07-26 10:31:15 +0200

    Cleaning unused configs and scripts

--- scripts/consumers/listen_wikipedia.failed.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-docker exec connect kafka-avro-console-consumer \
-  --property schema.registry.url=https://schemaregistry:8085 \
-  --property schema.registry.ssl.truststore.location=/etc/kafka/secrets/kafka.client.truststore.jks \
-  --property schema.registry.ssl.truststore.password=confluent \
-  --bootstrap-server kafka1:9091 --topic wikipedia.failed \
-  --consumer.config /etc/kafka/secrets/client_without_interceptors.config
