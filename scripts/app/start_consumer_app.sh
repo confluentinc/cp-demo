22da40e8718e9c57b1157d8a5fd3575824664c36
imcdo <imcdonald@confluent.io>
2021-08-31 17:13:12 -0700

    use wildcard

--- scripts/app/start_consumer_app.sh
+++ scripts/app/start_consumer_app.sh
@@ -9,8 +9,7 @@ fi
 ID=$1
 
 
-docker exec connect bash -c "export CLASSPATH=\$(ls /usr/share/java/monitoring-interceptors/monitoring-interceptors-*.jar); \
-   kafka-avro-console-consumer \
+docker exec connect kafka-avro-console-consumer \
    --bootstrap-server kafka1:11091,kafka2:11092 \
    --topic wikipedia.parsed \
    --consumer-property security.protocol=SSL \
@@ -32,7 +31,7 @@ docker exec connect bash -c "export CLASSPATH=\$(ls /usr/share/java/monitoring-i
    --consumer-property confluent.monitoring.interceptor.ssl.keystore.password=confluent \
    --consumer-property confluent.monitoring.interceptor.ssl.key.password=confluent \
    --consumer-property group.id=app \
-   --consumer-property client.id=consumer_app_$ID" > /dev/null 2>&1 &
+   --consumer-property client.id=consumer_app_$ID > /dev/null 2>&1 &
 
 #docker exec connect kafka-avro-console-consumer \
 #   --bootstrap-server kafka1:12091,kafka2:12092 \
