c591304bc8f4179bb94bc3800ea8a8add4d27f0b
Brett Randall <brett.randall@confluent.io>
2025-07-21 07:43:59 +1000

    Remove commented-out missing-jq code - cp-demo requires jq.

--- scripts/helper/control-center-modifications.sh
+++ scripts/helper/control-center-modifications.sh
@@ -3,10 +3,7 @@
 # With RBAC enabled, C3 communication thru its REST endpoint requires JSON Web Tokens (JWT)
 JWT_TOKEN=$(curl -s -u controlcenterAdmin:controlcenterAdmin http://localhost:9021/api/metadata/security/1.0/authenticate | jq -r .auth_token)
 
-# If you have 'jq'
 clusterId=$(curl -s -X GET -H "Authorization: Bearer ${JWT_TOKEN}" http://localhost:9021/2.0/clusters/kafka/ | jq --raw-output '.[0].clusterId')
-# If you don't have 'jq'
-#clusterId=$(curl -s -X GET -H "Authorization: Bearer ${JWT_TOKEN}" http://localhost:9021/2.0/clusters/kafka/ | awk -v FS="(clusterId\":\"|\",\"displayName)" '{print $2}')
 
 echo -e "\nRename the cluster in Control Center from ${clusterId} to Kafka Raleigh"
 curl -X PATCH -H "Authorization: Bearer ${JWT_TOKEN}" -H "Content-Type: application/merge-patch+json" -d '{"displayName":"Kafka Raleigh"}' http://localhost:9021/2.0/clusters/kafka/$clusterId
